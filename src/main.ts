2543fd875c34f2869a3904761df9a41bf792322a
ripuns <ripunsethia1668@gmail.com>
2026-08-06 21:35:26 +0530

    feat(core): add validation with class-transformer and class-validator and add global exception-handling by creating a filter file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    // check all reqs before your route handler does
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true, // strings into bools, nos
+    }),
+  );
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.enableShutdownHooks();
   const configService = app.get(ConfigService);
   app.setGlobalPrefix('/v1');
   const port = configService.get<number>('PORT', 3000);
