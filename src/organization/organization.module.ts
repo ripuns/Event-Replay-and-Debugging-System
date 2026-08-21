327e02118671098656796275f3636f2aed50bfe1
ripuns <ripunsethia1668@gmail.com>
2026-08-21 23:44:09 +0530

    fix(organization): correct module structure and formatting in OrganizationModule

--- src/organization/organization.module.ts
+++ src/organization/organization.module.ts
@@ -1,9 +1,14 @@
-import { OrganizationService } from "./organization.service";
-import { OrganizationRepository } from "./organization.repository";
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
+import { OrganizationService } from './organization.service';
+import { OrganizationRepository } from './organization.repository';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-    controllers: [OrganizationRepository],
-    providers: [OrganizationService],
+  imports: [PrismaModule],
+  providers: [
+    OrganizationService,
+    OrganizationRepository,
+  ],
+  exports: [OrganizationService],
 })
 export class OrganizationModule {}
