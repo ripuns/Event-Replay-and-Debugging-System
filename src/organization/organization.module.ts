c295ecbb07e969b7bf1adf0f621d997270fedda2
ripuns <ripunsethia1668@gmail.com>
2026-09-01 08:47:11 +0530

    refactor(modules): integrate controllers and guards into organization and projects modules

--- src/organization/organization.module.ts
+++ src/organization/organization.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { OrganizationService } from './organization.service';
 import { OrganizationRepository } from './organization.repository';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ApiKeyModule } from '../api-keys/api-keys.module';
+import { ProjectsModule } from '../projects/projects.module';
+import { OrganizationController } from './organization.controller';
+import { ApiKeyGuard } from '../common/auth/api-key.guard';
 
 @Module({
-  imports: [PrismaModule],
-  providers: [
-    OrganizationService,
-    OrganizationRepository,
-  ],
+  imports: [PrismaModule, ApiKeyModule, ProjectsModule],
+  controllers: [OrganizationController],
+  providers: [OrganizationService, OrganizationRepository, ApiKeyGuard],
   exports: [OrganizationService],
 })
 export class OrganizationModule {}
--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,15 +1,23 @@
-import { Module } from "@nestjs/common";
-import { ProjectsRepository } from "./projects.repository";
-import { ProjectsService } from "./projects.service";
-import { PrismaService } from "src/prisma/prisma.service";
+import { Module } from '@nestjs/common';
+import { ProjectsRepository } from './projects.repository';
+import { ProjectsService } from './projects.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { ApiKeyModule } from '../api-keys/api-keys.module';
+import { ProjectsController } from './projects.controller';
+import { ApiKeyGuard } from '../common/auth/api-key.guard';
+import { ProjectAccessGuard } from '../common/auth/project-access.guard';
+import { OrganizationAccessGuard } from '../common/auth/organization-access.guard';
 
 @Module({
-    providers: [
-        ProjectsService,
-        ProjectsRepository,
-        PrismaService
-    ],
-    exports: [ProjectsService]
+  imports: [PrismaModule, ApiKeyModule],
+  controllers: [ProjectsController],
+  providers: [
+    ProjectsService,
+    ProjectsRepository,
+    ApiKeyGuard,
+    ProjectAccessGuard,
+    OrganizationAccessGuard,
+  ],
+  exports: [ProjectsService, ProjectsRepository],
 })
-
-export class ProjectsModule{}
+export class ProjectsModule {}
