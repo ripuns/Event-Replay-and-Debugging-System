b9e38bd69f2d22fc354c833be805fd2fa3d71e9c
ripuns <ripunsethia1668@gmail.com>
2026-09-09 22:02:59 +0530

    feat(events): import SnapshotsModule

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -4,11 +4,12 @@ import { EventsService } from './events.service';
 import { EventsRepository } from './events.repository';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ApiKeyModule } from '../api-keys/api-keys.module';
+import { SnapshotsModule } from '../snapshots/snapshots.module';
 import { ApiKeyGuard } from '../common/auth/api-key.guard';
 import { ProjectAccessGuard } from '../common/auth/project-access.guard';
 
 @Module({
-  imports: [PrismaModule, ApiKeyModule],
+  imports: [PrismaModule, ApiKeyModule, SnapshotsModule],
   controllers: [EventsController],
   providers: [EventsService, EventsRepository, ApiKeyGuard, ProjectAccessGuard],
   exports: [EventsService],
